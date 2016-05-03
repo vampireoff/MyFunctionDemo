600971d7ac8d5b6b7eae65f8def0a18b932f8924
vampireoff <452517420@qq.com>
2016-05-03 16:53:10 +0800

    0.0

--- src/com/function/utils/MyViewUtil.java
+++ src/com/function/utils/MyViewUtil.java
@@ -4,7 +4,13 @@ import java.util.List;
 import java.util.Map;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -12,6 +18,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.function.activity.MainActivity;
+import com.function.activity.R;
 import com.function.views.MyAlertDialog;
 import com.function.views.MyAlertDialog.AlertCallback;
 import com.function.views.MyDateDialog;
@@ -33,6 +41,29 @@ import com.function.views.MyTimeSelectDialog.AlertTimeCallback;
  *
  */
 public class MyViewUtil {
+	
+	/**
+	 * 显示notify
+	 * @param context
+	 */
+	public static void showNotify(Context context){
+		NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context); 
+        
+        mBuilder.setContentTitle("SMSservice runing...")//设置通知栏标题  
+        .setTicker("runing") //通知首次出现在通知栏，带上升动画效果的  
+        .setContentIntent(PendingIntent.getActivity(
+        		context, 110, new Intent(context, MainActivity.class), 1))
+        .setWhen(System.currentTimeMillis())//通知产生的时间，会在通知信息里显示，一般是系统获取到的时间  
+        .setPriority(Notification.PRIORITY_DEFAULT) //设置该通知优先级  
+        .setAutoCancel(false)//设置这个标志当用户单击面板就可以让通知将自动取消    
+        .setOngoing(true)//ture，设置他为一个正在进行的通知。他们通常是用来表示一个后台任务,用户积极参与(如播放音乐)或以某种方式正在等待,因此占用设备(如一个文件下载,同步操作,主动网络连接)  
+//        .setDefaults(Notification.DEFAULT_SOUND)//向通知添加声音、闪灯和振动效果的最简单、最一致的方式是使用当前的用户默认设置，使用defaults属性，可以组合  
+        //Notification.DEFAULT_ALL  Notification.DEFAULT_SOUND 添加声音 // requires VIBRATE permission  
+        .setSmallIcon(R.drawable.ic_launcher);//设置通知小ICON  
+        
+        notificationManager.notify(110, mBuilder.build());
+	}
 	
 	/**
 	 * 显示提示对话框
